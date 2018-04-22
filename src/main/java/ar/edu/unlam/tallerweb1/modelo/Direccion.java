3442bd626fc26ca44075ea78fcc408ea180f2c1e
Ariel Martin <arielmartin80@gmail.com>
2018-04-22 16:17:32 -0300

    se agregaron los modelos de Farmacia y Direccion

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
@@ -0,0 +1,57 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.*;
+
+@Entity
+public class Direccion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String calle;
+	private int numero;
+
+//	@ManyToOne
+//	private Barrio barrio;
+	
+	public Direccion(String calle, int numero) {
+		this.calle = calle;
+		this.numero = numero;
+	}
+
+	public Direccion() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+//	public Barrio getBarrio() {
+//		return barrio;
+//	}
+//	public void setBarrio(Barrio barrio) {
+//		this.barrio = barrio;
+//	}
+	
+}
---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Farmacia.java
@@ -0,0 +1,84 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.*;
+
+@Entity
+public class Farmacia {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	private String telefono;
+	private String diaDeTurno;
+
+	@OneToOne
+	private Direccion direccion;
+	
+//	@OneToOne
+//	private Punto punto;
+//	
+//	public Punto getGeoLocalizacion() {
+//		return punto;
+//	}
+//
+//	public void setGeoLocalizacion(Punto punto) {
+//		this.punto = punto;
+//	}
+//	
+//	public void setGeoLocalizacion(String latitud, String longitud) {
+//		this.punto.setLatitud(latitud);
+//		this.punto.setLongitud(longitud);
+//	}
+
+	public Farmacia(String nombre,String diaDeTurno) {
+		this.nombre=nombre;
+		this.diaDeTurno=diaDeTurno;
+	}
+	
+	public Farmacia(Direccion direccion) {
+		this.direccion=direccion;
+	}
+	
+	public Farmacia() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDiaDeTurno() {
+		return diaDeTurno;
+	}
+	public void setDiaDeTurno(String diaDeTurno) {
+		this.diaDeTurno = diaDeTurno;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+		
+	}
+	
+	
+	
+	
+}
